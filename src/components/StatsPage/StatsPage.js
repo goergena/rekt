ffe51bf5612e7db9f2af769eb1638e0f3de4662a
framezach <framez@bgsu.edu>
2018-06-23 12:51:22 -0400

    Stats Page

---
+++ src/components/StatsPage/StatsPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div className="StatsPage">
+        <h2>Stats Page</h2>
+        </div>
+    );
+  }
+}
+
+export default StatsPage;
