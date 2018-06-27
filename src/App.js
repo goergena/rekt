e77eaabfcf372135a2fb620481659e2a7e3b8779
Amanda <gunnelsamanda19@gmail.com>
2018-06-26 20:21:27 -0400

    Test homepage presence

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
+        Test
         <Route exact path="/" component={Home} />
           <Route path="/login" component={Login} />
           <Route path="/stats" component={Stats} />
