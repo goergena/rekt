5866da7cee1af88ec833696bef67a0a28e54482d
Amanda <gunnelsamanda19@gmail.com>
2018-06-28 14:47:10 -0400

    Partial commit to Home Page an new directory

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,11 +4,19 @@ class HomePage extends Component {
   render() {
     return (
       <div className="HomePage">
-        <h2>Home Page</h2>
-        <a href="/login">Login Page</a>
-        <a href="/statstop">Stats Top</a>
-        <a href="/stats">Stats</a>
-        </div>
+        <h2>REKT</h2>
+        <a href="/login">Login</a>
+        <a href="/statstop">Stats</a>
+        <a href="/buySell">Buy & Sell Equipment</a>
+        
+
+<a class="m-microphone"><i class="material-icons" style="font-size:26px;">mic</i></a>
+<a class="m-content">Announcement Content</a>
+{/* <a class="m-close" href="#"><i class="material-icons">close</i></a> */}
+</div>
+
+
+
     );
   }
 }
