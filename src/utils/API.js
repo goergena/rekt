b9b6b3ce643f753a1b5322a785a9b9d2ff7e8e20
Anthony Mahramus <anthony.mahramus@gmail.com>
2018-07-11 23:59:10 -0400

    Fixed axios syntax

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 export default {
   // Moved from BowlingPage.js
-  getBowling = function() {
+  getBowling: function() {
       return axios.get('/api/sports/bowling');
   },
   // Moved from BowlingPage.js
-  getLeagueList = function() {
+  getLeagueList: function() {
       return axios.get('api/townsports/1');
   }
 };
