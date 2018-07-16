fbf845cc9a8d67964028ca15efb7ad7f6144ecfa
Amanda <gunnelsamanda19@gmail.com>
2018-07-16 14:32:16 -0400

    images added

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Jumbotron.css';
 import Bowling from '../../images/Bowling.jpg'
+import Wood from '../../images/Wood.jpg'
 
 const items = [
   {
     src: Bowling,
+    altText: '',
+    caption: ''
+  },
+  {
+    src: Wood,
     altText: '',
     caption: ''
   }
