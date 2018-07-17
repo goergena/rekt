22dbfffe96b5ce68c477ac429b4946ec231885bd
goergena <goergena@users.noreply.github.com>
2018-07-17 17:16:33 -0400

    pasted some of amandas code in

--- src/components/LeagueItem.js
+++ src/components/LeagueItem.js
@@ -1,20 +1,54 @@
-import React from 'react';
-import { connect } from 'react-redux';
-
-//right now these are just list item things but it would be good for them to be cards
-//or something prettier
-///and then each one will link to league:leagueid or something. set up react router in app.js
-
-const LeagueItem = ( { name, id, dispatch}) => (
-    <div>
-        <p>{amount} - {createdAt}</p>
-        <button className='btn btn-primary' onClick={() => {
-            dispatch(setFilterId({ id }));
-        }}>{name}</button>
-    </div>
-)
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 
+class LeagueItem extends Component {
+    handleClick = () => {
+        const { id, addToCart, removeFromCart, isInCart } = this.props;
+
+        if (isInCart) {
+            removeFromCart(id);
+        } else {
+            addToCart(id);
+        }
+    }
+
+    render() {
+        const { name, price, currency, image, isInCart } = this.props;
+
+        return (
+            <div className="product thumbnail">
+                <img src={image} alt="product" />
+                <div className="caption">
+                    <h3>{name}</h3>
+                    <div className="product__price">{price} {currency}</div>
+                    <div className="product__button-wrap">
+                        <button
+                            className={isInCart ? 'btn btn-danger' : 'btn btn-primary'}
+                            onClick={this.handleClick}
+                        >
+                            {isInCart ? 'Remove' : 'Add to cart'}
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+Product.propTypes = {
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number,
+    currency: PropTypes.string,
+    image: PropTypes.string,
+    isInCart: PropTypes.bool.isRequired,
+    addToCart: PropTypes.func.isRequired,
+    removeFromCart: PropTypes.func.isRequired,
+}
+
+export default LeagueItem;
 
 
-export default connect()(LeagueItem);
+
+
