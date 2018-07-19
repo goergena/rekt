0a528bf32bcc3d0b85ac4ad78955d57de2ee0876
Amanda <gunnelsamanda19@gmail.com>
2018-07-19 18:05:10 -0400

    style and cart added

--- src/App.js
+++ src/App.js
@@ -10,10 +10,11 @@ import TeamNav from './components/StatsPage/TeamNav.js'
 import Wrapper from './components/Wrapper/Wrapper.js'
 
 import Add from './components/Add/Add.js';
+import Shop from './components/Shop.js';
 
-const Home = () => ( <HomePage />);
+// const Home = () => ( <HomePage />);
 
-const Table = () => ( <TeamTable />)
+// const Table = () => ( <TeamTable />)
 
 
 
@@ -28,11 +29,12 @@ class App extends Component {
         <Wrapper>
           <Router>
             <div className="App">
-              <Route exact path = "/" component={Home} />
-              <Route path="/shop" component={ProductList} />
-              <Route path ="/teams" component={Table} />
+              <Route exact path = "/" component={HomePage} />
+              {/* <Route path="/shop" component={ProductList} /> */}
+              <Route path ="/teams" component={TeamTable} />
               <Route path ="/teamnav" component={TeamNav} />
               <Route path ="/add" component={Add} />
+              <Route path="/shop" component={Shop} />
             </div>
           </Router>
         </Wrapper>
