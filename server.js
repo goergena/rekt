87f17c0e0812ed2a78e0ff010a46cb162a5f5b47
Alyssa (Ali) Goergen <alyssa.goergen@gmail.com>
2018-09-18 12:16:14 -0400

    changed PORT on server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const routes = require("./routes/get-routes");
 const db = require("./models");
 
 const app = express();
-const PORT = process.env.PORT || 8301;
+const PORT = process.env.PORT || 3000;
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
